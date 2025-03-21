aad3a0705069739e0ee27edf30a72c4b70a9b2c4
yoseop-yeom <166366019+yoseop-yeom@users.noreply.github.com>
2025-03-21 15:30:15 +0900

    test sample

---
+++ src/main/java/com/example/ec2springbootsample/Ec2SpringBootSampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.ec2springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2SpringBootSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Ec2SpringBootSampleApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/ec2springbootsample/Ec2SpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2springbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2SpringBootSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
